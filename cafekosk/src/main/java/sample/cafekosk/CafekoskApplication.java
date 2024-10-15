17020a2afeae0ebbff70bbbac52cfe22101e440b
JUNGHOON AN <gnstm831@naver.com>
2024-10-15 20:31:26 +0900

    섹션3. 단위 테스트
    2. 프로젝트 세팅

---
+++ cafekosk/src/main/java/sample/cafekosk/CafekoskApplication.java
@@ -0,0 +1,13 @@
+package sample.cafekosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafekoskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafekoskApplication.class, args);
+    }
+
+}
